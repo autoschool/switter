e9411d42b3f9e6edb190518741343fb6844dbba4
charlie <charlie@yandex-team.ru>
2014-11-17 18:07:58 +0300

    Add properties

--- src/main/java/ru/qatools/school/switter/service/DatabaseProvider.java
+++ src/main/java/ru/qatools/school/switter/service/DatabaseProvider.java
@@ -4,6 +4,7 @@ import org.flywaydb.core.Flyway;
 import org.javalite.activejdbc.Base;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.qatools.school.switter.config.ServerConfig;
 
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
@@ -11,7 +12,6 @@ import javax.ws.rs.ext.Provider;
 import java.io.IOException;
 
 import static java.lang.String.format;
-import static java.nio.file.Files.createTempDirectory;
 
 /**
  * eroshenkoam
@@ -25,7 +25,8 @@ public class DatabaseProvider implements ContainerRequestFilter {
 
     static {
         try {
-            dbUrl = format("jdbc:h2:file:%s/%s,user=%s", getDbPath(), getDbName(), DBUSER);
+            ServerConfig config = ServerConfig.newInstance();
+            dbUrl = format("jdbc:h2:file:%s/%s,user=%s", config.getDbPath(), config.getDbName(), DBUSER);
             logger.info(format("Starting embedded database with url '%s' ...", dbUrl));
             openConnection();
             Flyway flyway = new Flyway();
@@ -41,19 +42,6 @@ public class DatabaseProvider implements ContainerRequestFilter {
         if (!Base.hasConnection()) {
             Base.open(org.h2.Driver.class.getName(), dbUrl, DBUSER, "");
         }
-    }
-
-    private static String getDbName() {
-        return getProperty("db.name", "default");
-    }
-
-    private static String getDbPath() throws IOException {
-        return getProperty("db.path", createTempDirectory("blog").toAbsolutePath().toString());
-    }
-
-    private static String getProperty(String key, String defaultValue) {
-        final String value = System.getProperty(key);
-        return (value == null) ? defaultValue : value;
     }
 
     @Override
